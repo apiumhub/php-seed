a078ebc86b090749e5ce1822063b6bb5b668df48
christian.ciceri <christian.ciceri@gmail.com>
2015-06-16 18:27:28 +0200

    first commit

---
+++ tests/FirstTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: christian
+ * Date: 30/09/14
+ * Time: 15:53
+ */
+
+namespace tests;
+
+
+class FirstTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
