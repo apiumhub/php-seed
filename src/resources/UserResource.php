8980366b4344cf84724174957cfef91c4e305662
christian.ciceri <christian.ciceri@gmail.com>
2015-06-23 13:01:01 +0200

    working server and functional test

---
+++ src/resources/UserResource.php
@@ -0,0 +1,18 @@
+<?php
+namespace resources;
+/**
+ * Created by IntelliJ IDEA.
+ * User: christian
+ * Date: 23/06/15
+ * Time: 12:12
+ */
+class UserResource
+{
+    public static function add($app)
+    {
+        $app->get('/hello/:name', function ($name) {
+            echo "Hello, $name";
+        });
+    }
+
+}
