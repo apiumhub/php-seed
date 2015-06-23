1ccf182463e3bcf03f7ac5d7dbbee6b2477ebe51
christian.ciceri <christian.ciceri@gmail.com>
2015-06-23 13:32:08 +0200

    autoload for slim

--- src/index.php
+++ src/index.php
@@ -1,9 +1,9 @@
 <?php
 
 require '../vendor/autoload.php';
-require "../vendor/slim/slim/Slim/Slim.php";
-\Slim\Slim::registerAutoloader();
+
 use resources\UserResource;
+
 $app = new \Slim\Slim();
 UserResource::add($app);
 $app->run();
