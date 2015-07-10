aea0fae6e193a3f2cb42ecf1089871553610bc80
christian.ciceri <christian.ciceri@gmail.com>
2015-07-10 16:35:59 +0200

    modified script

--- countLinesOfCode.sh
+++ countLinesOfCode.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #for file in $(git --git-dir="$folder" ls-tree --name-only -r HEAD); do git --git-dir="$folder" show HEAD:"$file" | grep -v "^\s+'.*"; done | wc -l
-for file in $(git --git-dir="$folder" ls-tree --name-only -r HEAD | grep -v ".*\.md\|txt"); do git --git-dir="$folder" show HEAD:"$file" | grep -v "^\s*//\|#.*"| grep -v '^[[:space:]]*$'; done | wc -l
+for file in $(git --git-dir=".git" ls-tree --name-only -r HEAD | grep -v ".*\.md\|txt"); do git --git-dir=".git" show HEAD:"$file" | grep -v "^\s*//\|#.*"| grep -v '^[[:space:]]*$'; done | wc -l
 
 
 
