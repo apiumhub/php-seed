bc9602bbf0472448ddfdd61cd5a3a29af8b4500e
christian.ciceri <christian.ciceri@gmail.com>
2015-07-10 16:35:05 +0200

    added script

---
+++ countLinesOfCode.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+folder=${1:-./DigitalPersonaTest.git}
+#for file in $(git --git-dir="$folder" ls-tree --name-only -r HEAD); do git --git-dir="$folder" show HEAD:"$file" | grep -v "^\s+'.*"; done | wc -l
+for file in $(git --git-dir="$folder" ls-tree --name-only -r HEAD | grep -v ".*\.md\|txt"); do git --git-dir="$folder" show HEAD:"$file" | grep -v "^\s*//\|#.*"| grep -v '^[[:space:]]*$'; done | wc -l
+
+
+
