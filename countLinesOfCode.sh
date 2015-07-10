91e39d3cf85f638726b3b355fa077f9b6902d380
christian.ciceri <christian.ciceri@gmail.com>
2015-07-10 16:32:19 +0200

    changed sheeban

--- countLinesOfCode.sh
+++ countLinesOfCode.sh
@@ -1,4 +1,4 @@
-#/bin/bash -e
+#!/usr/bin/env bash
 folder=${1:-./DigitalPersonaTest.git}
 echo -n "$folder: "
 #for file in $(git --git-dir="$folder" ls-tree --name-only -r HEAD); do git --git-dir="$folder" show HEAD:"$file" | grep -v "^\s+'.*"; done | wc -l
