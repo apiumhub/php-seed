2dd767376215252c2f87d4dbcabcb8b6a802622f
christian.ciceri <christian.ciceri@gmail.com>
2015-09-18 18:42:40 +0200

    added inpection to build + added to remove private variable name convention

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 cp build/patches/SqlServerAdapter.php vendor/robmorgan/phinx/src/Phinx/Db/Adapter/SqlServerAdapter.php
+rm -fr vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/Zend/Sniffs/NamingConventions/ValidVariableNameSniff.php
 . build/scripts/migrate_up.sh
 #comment
 . test.sh
+./vendor/phing/phing/bin/phing inspect
